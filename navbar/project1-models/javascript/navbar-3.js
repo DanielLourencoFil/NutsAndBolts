7257a6adb78a86f4a96e0ab98f6a9cda4db9e33a
daniellourencoedu <daniellourencoedu@gmail.com>
2021-12-21 00:36:34 -0300

    shadow-box remove feature and small typo fixes

--- navbar/project1-models/javascript/navbar-3.js
+++ navbar/project1-models/javascript/navbar-3.js
@@ -1,13 +1,16 @@
 // setup nav
 const navBtn = document.getElementById("burguer-btn");
 const navbar = document.getElementById("nav-menu");
+const navShadow = document.querySelector("nav");
 const navbar2 = document.getElementById("nav-menu2");
 const navExit = document.getElementById("exit-btn")
-// const navClose = document.getElementById(".burguer-btn");
+
 // show nav
 navBtn.addEventListener("click", function () {
     navbar.classList.toggle("show-links");
     navbar2.classList.toggle("show-links");
+     navShadow.classList.toggle("remove-shadow");
+   
 });
 
 // navExit.
